145c4bc97cd201e0dd7bc2ce9b2699dff80bb624
Simon Kardell <simonkardell@gmail.com>
2018-05-07 10:22:37 +0200

    added template code for ddpg

--- model/ddpg.py
+++ model/ddpg.py
@@ -1,6 +1,22 @@
 import tensorflow as tf
-import tensorflow.contrib as tfc
+
+from model.actor import Actor
+from model.critic import Critic
+from model.exploration import Exploration
 
 
-class DDPG(object):
-    def __init__(self):
+class DDPG(Exploration):
+    def __init__(self, init_std, final_std):
+        Exploration.__init__(self, init_std, final_std)
+        self.sess = tf.Session()
+        self._actor = Actor()
+        self._critic = Critic()
+
+    def action(self, state):
+        pass
+
+    def add_to_replay(self, state, action, next_state, reward, terminal):
+        pass
+
+    def train(self, batch_size=64):
+        pass
