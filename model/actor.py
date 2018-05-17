e4cdec722c203e2f863ba99a0e0ae37d44fcc1f5
Simon Kardell <simonkardell@gmail.com>
2018-05-17 09:31:02 +0200

    add docstrings for actor

--- model/actor.py
+++ model/actor.py
@@ -1,10 +1,34 @@
 import tensorflow as tf
 import tensorflow.contrib.layers as tfcl
-import numpy as np
 
 
 class Actor(object):
+    """Attributes
+    ----------
+    trainable_vars : List<Tensor>
+        List of trainable variables
+    action : Tensor
+        Policy-network endpoint
+    state: Tensorflow placeholder
+        Input to model
+    """
+
     def __init__(self, state_dim, action_dim, scope="actor", action_bounds=(-2., 2.)):
+        """Builds the policy graph
+
+        Parameters
+        ----------
+        state_dim : int
+            dimensionality of the state.
+        action_dim : int
+            dimensionality of the action.
+        scope : str, optional
+            variable scope for the network (the default is "actor")
+        action_bounds : tuple, optional
+            max and min value ofd the actions (the default is (-2., 2.))
+
+        """
+
         self.action_bounds = action_bounds
         self.action_dim = action_dim
         with tf.variable_scope(scope):
@@ -16,6 +40,18 @@ class Actor(object):
             self._trainable_vars = tf.trainable_variables(scope)
 
     def _model(self, scope):
+        """Adds the policy-network to the graph.
+
+        Parameters
+        ----------
+        scope : string
+            variable scope for the network
+        Returns
+        -------
+        Tensor
+            action
+        """
+
         act = tf.nn.elu
         initializer = tfcl.variance_scaling_initializer()
         out_init = tfcl.variance_scaling_initializer(factor=.1)
